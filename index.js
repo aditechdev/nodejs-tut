57f2b1bf7b93c1d1284393ca86ee300366b72e2d
Aditya Anand <aditechdevofficial@gmail.com>
2023-03-13 22:02:01 +0530

    :sparkles: OS LEVEL NODE

--- index.js
+++ index.js
@@ -1,78 +1,9 @@
-const express = require('express');
-const multer = require('multer');
-require('./config');
-const Product = require('./product');
+const os = require('os');
 
-const app = express();
-app.use(express.json());
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) { 
-            cb(null, "uploads")
-        }, 
-        filename: function (req, file, cb) { 
-
-            cb(null, file.fieldname+"-"+Date.now()+".jpg")
-        }
-    })
-}).single("user_file");
-
-app.post('/create', async(req, res) => {
-    let data = new Product(req.body);
-    let result =await data.save();
-    console.log(result);
-    res.send(result);
-
-})
-app.get('/list', async(req, res) => {
-    let data =await Product.find();
-    // let result =await data.save();
-    // console.log(result);
-    res.send(data);
-
-})
-app.delete('/delete/:_id', async (req, res) => {
-    // console.log(req.params);
-    let data = await Product.deleteOne(req.params);
-    // let result =await data.save();
-    // console.log(result);
-    res.send(data);
-
-})
-app.put('/update/:_id', async(req, res) => {
-    let data = await Product.updateOne(req.params, {
-        $set: req.body });
-    // let result =await data.save();
-    // console.log(result);
-    res.send(data);
-
-})
-
-app.get('/search/:key', async (req, res) => {
-    console.log(req.params.key);
-    let data = await Product.find(
-        {
-            "$or": [
-                { "name": {$regex:req.params.key} },
-                { "brand": {$regex:req.params.key} }
-            ]
-        }
-    );
-    res.send(data);
-})
-
-// app.post('upload', await(req, res)=> {
-    
-// })
-
-
- 
-app.post('/upload', upload, async (req, res) => {
-    res.send("DONE");
-    
-
-});
-
-
-app.listen(4700)
+// console.log(os);
+// console.log(os.arch());
+// console.log(os.freemem()/(1024*1024*1024));
+// console.log(os.totalmem()/(1024*1024*1024));
+// console.log(os.hostname());
+// console.log(os.platform());
+console.log(os.userInfo());
